85b4fcf7ba205e937b6cd80420d916e18c049804
Anuj pal <148964497+Anuj2Code@users.noreply.github.com>
2024-03-11 20:59:09 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -14,14 +14,9 @@ require('dotenv').config()
 var cors = require('cors') 
 const app = express();
 const base = process.env.base || 8800;
-
 connectToMongo()
 const port = base;
-app.use(cors({
-    origin:["https://mern-api-sand.vercel.app/"],
-    methods:["POST","GET","DELETE","PUT"],
-    credentials:true
-}));
+app.use(cors());
 app.use(cookieParser());  
 app.use(bodyParser.urlencoded({ extended: true}));
 app.use(express.json({limit: '50mb'}));
@@ -29,10 +24,6 @@ app.use(express.json({limit: '50mb'}));
 app.use(fileUpload({
  useTempFiles:true
 }));
-
-app.get('/',(req,res)=>{
-  res.send('vcdt')
-})
 
 app.use('/api/auth',authRoute);
 app.use('/api/item',proRoute);
